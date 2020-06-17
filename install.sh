3fe0267e8ed31f7aab29ac1e847261ee54fbc002
Mateusz Drab <mateuszd@mpd.pw>
2020-06-17 14:37:25 +0100

    Log bash output from go

--- install.sh
+++ install.sh
@@ -15,8 +15,9 @@ apt-get install -y docker-compose
 # Ensure Docker daemon is running in swarm mode
 docker swarm init
 
-# Get the latest franela/dind image
+# Get the latest franela images
 docker pull franela/dind
+docker pull franela/k8s
 
 #Install Go
 wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
@@ -29,11 +30,12 @@ cd play-with-docker
 # so that no network requests are required within the containers.
 # The module cache is retained in the pwd and l2 containers so the
 # download is a one-off if you omit this step.
-bash -c '/.go/bin/go mod vendor'
+bash -c '/.go/bin/go mod vendor' > /var/log/go.log 2>&1
 
 # Set correct branch
 if [ $1 == "k8s" ]; then
-  git checkout -t "origin/$1"
+  mv www/default www/docker
+  mv www/k8s www/default
 fi
 
 # Start PWD as a container
